eb9a7a8c03b6d1b977cfb9fcaa8a973cafb43365
Do Minh Hai <dominhhai1111@gmail.com>
2018-05-14 17:31:25 +0700

    add detail

--- app/src/main/java/com/example/admin/playvideourl/FilmDetailActivity.java
+++ app/src/main/java/com/example/admin/playvideourl/FilmDetailActivity.java
@@ -22,22 +22,30 @@ public class FilmDetailActivity extends AppCompatActivity {
     Button btnWatch;
     Button btnAddToFavorite;
     ListView lvComment;
-    String film_url;
+
+    Film film;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_film_detail);
         Intent intent1 = getIntent();
-        film_url = intent1.getStringExtra("film_url");
+        film = (Film) intent1.getSerializableExtra("film");
 
         btnWatch = findViewById(R.id.btnWatch);
         btnWatch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent2 = new Intent(FilmDetailActivity.this, MainActivity.class);
-                intent2.putExtra("film_url", film_url);
+                intent2.putExtra("film_url", film.getUrl());
                 startActivity(intent2);
             }
         });
+
+        txtName = findViewById(R.id.txtName);
+        txtName.setText(film.getName());
+        txtTime = findViewById(R.id.txtTime);
+        txtTime.setText(film.getTime());
+        txtDetail = findViewById(R.id.txtDetail);
+        txtDetail.setText(film.getDetail());
     }
 }
